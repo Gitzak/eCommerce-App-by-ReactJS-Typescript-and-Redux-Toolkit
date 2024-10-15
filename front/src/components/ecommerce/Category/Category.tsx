70082dd42023924efac0d9eb1b87c49d3a128a3f
GitZak <zakaria.belrhali@gmail.com>
2024-10-15 17:14:09 +0100

    display categ and products

--- front/src/components/ecommerce/Category/Category.tsx
+++ front/src/components/ecommerce/Category/Category.tsx
@@ -1,18 +1,19 @@
+import { TCategory } from "@customTypes/category";
+import { Link } from "react-router-dom";
 import styles from "./styles.module.css";
 const { category, categoryImg, categoryTitle } = styles;
 
-const Category = () => {
-    return (
-        <div className={category}>
-            <div className={categoryImg}>
-                <img
-                    src="https://cdn-eu.dynamicyield.com/api/9876644/images/244c68ad42d8b__hp-w12-22032022-h_m-women_shirts-blouses.jpg"
-                    alt=""
-                />
-            </div>
-            <h4 className={categoryTitle}>Title</h4>
+const Category = ({ title, img, prefix }: TCategory) => {
+  return (
+    <div className={category}>
+      <Link to={`/categories/products/${prefix}`}>
+        <div className={categoryImg}>
+          <img src={img} alt={title} />
         </div>
-    );
+        <h4 className={categoryTitle}>{title}</h4>
+      </Link>
+    </div>
+  );
 };
 
 export default Category;
