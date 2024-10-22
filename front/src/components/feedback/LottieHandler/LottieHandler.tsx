297776e7695933685cbda17370608bdc221341e4
GitZak <zakaria.belrhali@gmail.com>
2024-10-22 16:19:48 +0100

    fix LottieHandler

--- front/src/components/feedback/LottieHandler/LottieHandler.tsx
+++ front/src/components/feedback/LottieHandler/LottieHandler.tsx
@@ -1,14 +1,16 @@
 import Lottie from "lottie-react";
-import notFound from "@assets/lottieFiles/404.json";
+import notFound from "@assets/lottieFiles/notFound.json";
 import empty from "@assets/lottieFiles/empty.json";
 import loading from "@assets/lottieFiles/loading.json";
-import error from "@assets/lottieFiles/404.json";
+import error from "@assets/lottieFiles/error.json";
+import success from "@assets/lottieFiles/success.json";
 
 const lottieFilesMap = {
   notFound,
   empty,
   loading,
   error,
+  success,
 };
 
 type LottieHandlerProps = {
